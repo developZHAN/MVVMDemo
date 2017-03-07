65cc268e08037b98debcac20630984fe196caca5
詹瞻 <zhanzhan@zhanzhandemacbook-pro.local>
2017-03-07 20:25:19 +0800

    Initial Commit

---
+++ TYMVVMDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TYMVVMDemo
+//
+//  Created by 詹瞻 on 2017/3/7.
+//  Copyright © 2017年 developZHAN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TYMVVMDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TYMVVMDemo
+//
+//  Created by 詹瞻 on 2017/3/7.
+//  Copyright © 2017年 developZHAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
